b0bf864a6b6a574c0a8ea04717c0baec25ced45d
alanbalugu <55846088+alanbalugu@users.noreply.github.com>
2020-09-12 18:58:47 -0400

    Update alumni_test.py
    Updated the variable names so that it runs

--- api/models/alumni_test.py
+++ api/models/alumni_test.py
@@ -4,11 +4,11 @@ alumni_db.create_all()
 from alumni import *
 
 
-alan = Alumni(first_name = "Alan", last_name = "Balu")
-bob = Alumni(first_name = "Bob", last_name = "Joe")
+alan = AlumniModel(first_name = "Alan", last_name = "Balu")
+bob = AlumniModel(first_name = "Bob", last_name = "Joe")
 
-new_email1 = Email(email = "alan12@georgetown.edu")
-new_email2 = Email(email = "bob12@georgetown.edu")
+new_email1 = EmailModel(email = "alan12@georgetown.edu")
+new_email2 = EmailModel(email = "bob12@georgetown.edu")
 
 alan.emails.append(new_email1)
 bob.emails.append(new_email2)
@@ -18,6 +18,6 @@ alumni_db.session.add(bob)
 alumni_db.session.commit()
 
 
-print(Alumni.query.all())
+print(AlumniModel.query.all())
 
-print(Email.query.all())
+print(EmailModel.query.all())
